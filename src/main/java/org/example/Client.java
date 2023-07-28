7b5fcc5fe6971c4cb22d13afc5b07767de815c6a
denchik <denchik@mail.ru>
2023-07-28 16:46:31 +0300

    dd

---
+++ src/main/java/org/example/Client.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import org.example.grpc.GreetingServiceGrpc;
+import org.example.grpc.GreetingServiceOuterClass;
+
+public class Client {
+    public static void main(String[] args) {
+        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:8080")
+                .usePlaintext().build();
+        GreetingServiceGrpc.GreetingServiceBlockingStub stub =
+                GreetingServiceGrpc.newBlockingStub(channel);
+
+        GreetingServiceOuterClass.HelloRequest request = GreetingServiceOuterClass.HelloRequest
+                .newBuilder().setName("Neil").build();
+        GreetingServiceOuterClass.HelloResponce responce = stub.greeting(request);
+        System.out.println(responce);
+        channel.shutdownNow();
+    }
+}
